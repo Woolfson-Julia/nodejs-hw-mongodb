e391abc6381b533d3c03d130dbd79b96cb46a2b0
Woolf <woolfson1995@gmail.com>
2025-05-12 18:28:29 +0300

    hw2 node

---
+++ src/server.js
@@ -0,0 +1,73 @@
+import express from 'express';
+import pino from 'pino-http';
+import cors from 'cors';
+
+import { getEnvVar } from './utils/getEnvVar.js';
+import { getAllContacts, getContactById } from './services/contacts.js';
+import mongoose from 'mongoose';
+
+
+const PORT = Number(getEnvVar('PORT', '3000'));
+
+export const setupServer = () => {
+  const app = express();
+  app.use(express.json());
+  app.use(cors());
+  app.use(
+    pino({
+      transport: {
+        target: 'pino-pretty',
+      },
+    }),
+  );
+  app.get('/contacts', async (req, res) => {
+    const contacts = await getAllContacts();
+
+    res.status(200).json({
+      status: 200,
+      message: "Successfully found contacts!",
+      data: contacts,
+    });
+  });
+
+  app.get('/contacts/:contactId', async (req, res) => {
+
+    const { contactId } = req.params;
+    if (!mongoose.Types.ObjectId.isValid(contactId)) {
+      return res.status(400).json({
+        message: 'Invalid contact ID format'
+      });
+    }
+    const contact = await getContactById(contactId);
+
+    if (!contact) {
+      return res.status(404).json({
+        message: 'Contact not found'
+      });
+    }
+    res.status(200).json({
+      status: 200,
+      message: `Successfully found contact with id ${contactId}!`,
+      data: contact,
+    });
+  });
+
+  app.use((req, res, next) => {
+    res.status(404).json({
+      message: 'Not found',
+    });
+  });
+  app.use((err, req, res, next) => {
+    res.status(500).json({
+      message: 'Something went wrong',
+      error: err.message,
+    });
+  });
+  app.listen(PORT, (error) => {
+    if (error) {
+      throw error;
+    }
+
+    console.log(`Server is running on port ${PORT}`);
+  });
+};
