59b8ac3c90516e39e073e5fa191233cdf63df66f
Woolf <woolfson1995@gmail.com>
2025-06-13 21:59:53 +0300

    delete try catch

--- src/utils/saveFileToCloudinary.js
+++ src/utils/saveFileToCloudinary.js
@@ -4,18 +4,12 @@ import fs from 'node:fs/promises';
 import { getEnvVar } from './getEnvVar.js';
 import { CLOUDINARY } from '../constants/index.js';
 
-try {
-  cloudinary.v2.config({
-    cloud_name: getEnvVar(CLOUDINARY.CLOUD_NAME),
-    api_key: getEnvVar(CLOUDINARY.API_KEY),
-    api_secret: getEnvVar(CLOUDINARY.API_SECRET),
-  });
-} catch (err) {
-  console.warn(
-    'Cloudinary config failed to initialize at startup:',
-    err.message,
-  );
-}
+cloudinary.v2.config({
+  secure: true,
+  cloud_name: getEnvVar(CLOUDINARY.CLOUD_NAME),
+  api_key: getEnvVar(CLOUDINARY.API_KEY),
+  api_secret: getEnvVar(CLOUDINARY.API_SECRET),
+});
 
 export const saveFileToCloudinary = async (file) => {
   const response = await cloudinary.v2.uploader.upload(file.path);
